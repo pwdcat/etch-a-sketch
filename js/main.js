787dacdbd074c91f246c7fcda7588c3c11ef47a9
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-03 16:20:28 -0500

    Basic etch a sketch finished Add input box for size Sizes scale relatively Did color change logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 const container = document.querySelector('#container');
 
-let size = 16;
+createGrid(16);
 
-for(let i = 0; i < size; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(let j = 0; j < size; j++){
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        div.addEventListener('mouseenter', changeColor());
-        row.appendChild(div);
+function makeGrid(){
+    let size = document.getElementById("size").value;
+    if(size > 100){
+        alert("Size too big");
     }
-    container.appendChild(row);
+    else{
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+          }
+        createGrid(size)
+    }
 }
 
-function changeColor(){
-    
+function createGrid(size){
+    for(let i = 0; i < size; i++){
+        const row = document.createElement('div');
+        let px = 500/size;
+        row.classList.add('row');
+        row.style.height = `${px}px`;
+        for(let j = 0; j < size; j++){
+            const div = document.createElement('div');
+            div.classList.add('grid');
+            div.style.height = `${px}px`;
+            div.style.width = `${px}px`;
+            div.addEventListener('mouseenter', ()=>{
+                div.style.backgroundColor = 'black';
+            });
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+    }
 }
+
+
+
